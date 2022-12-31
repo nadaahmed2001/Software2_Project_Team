fb5658ba4950ae00cbd50aeb69de65239a3b524c
Nada Ahmed <60941223+nadaahmed2001@users.noreply.github.com>
2022-12-31 16:20:31 +0200

    Delete src/main/java/SystemUsers directory

--- src/main/java/SystemUsers/Subject.java
+++
@@ -1,7 +0,0 @@
-package com.softwarePhase2.se.softwarePhase2.SystemUsers;
-
-public interface Subject {
-	public void RegistRequest(observer o);
-	public void RemoveRequest(observer o);
-	public void notifyObservers();
-}
--- src/main/java/SystemUsers/observer.java
+++
@@ -1,8 +0,0 @@
-package com.softwarePhase2.se.softwarePhase2.SystemUsers;
-
-
-public interface observer {
-	
-	public void update(boolean messages);
-
-}
